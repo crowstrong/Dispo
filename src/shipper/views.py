189ddcaa321bfc5944d31f9b7bfc8ee1c8b58df6
Nikolay  Tokariev <104066410+crowstrong@users.noreply.github.com>
2022-10-25 14:14:31 +0300

    HW28 CI/CD FIX_5

--- src/shipper/views.py
+++ src/shipper/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from django.views.generic import CreateView
+# from shipper.models import Order
+# from django.urls import reverse_lazy
+#
+#
+# class CreateOrderView(CreateView):
+#     model = Order
+#     form_class = OrderForm
+#     template_name = "shipper/order_create.html"
+#     success_url = reverse_lazy("shipper:get_order")
