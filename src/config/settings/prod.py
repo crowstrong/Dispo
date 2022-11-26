18032537b4dc514410853d770ba0d9fc81dcb393
Nikolay  Tokariev <104066410+crowstrong@users.noreply.github.com>
2022-11-26 17:40:26 +0200

    prod ver

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -8,7 +8,7 @@ print(CURRENT_ENV)
 DEBUG = False
 # SECURITY WARNING: don't run with debug turned on in production!
 # TODO update later
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "ec2-44-207-0-126.compute-1.amazonaws.com"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = "/static/"
@@ -17,11 +17,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 DATABASES = {
-    "default_sql": {
+    "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     },
-    "default": {
+    "default_sql": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
